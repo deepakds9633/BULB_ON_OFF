d32b0a8bc75a02e6d5ebd4b33640d0430cb40af8
DEEPAK P <deepakpalanivel.p2006@gmail.com>
2025-03-19 16:29:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src="on.png"
+}
+function bulb_off()
+{
+    img.src="off.png"
+}
